8aae176eab021af16c4dc59afb52cd2aaaa85059
Swarup-3112 <swarupaa619yes@gmail.com>
2023-04-29 09:48:32 +0530

    final commit

--- constant.js
+++ constant.js
@@ -3,4 +3,5 @@ module.exports.Qr = {
     bigben: "http://localhost:5000/images/qr/bigben.png",
     arch: "http://localhost:5000/images/qr/arch.png",
     pyramid: "http://localhost:5000/images/qr/pyramid.png",
+    eiffel: "http://localhost:5000/images/qr/eiffel.png"
 }
